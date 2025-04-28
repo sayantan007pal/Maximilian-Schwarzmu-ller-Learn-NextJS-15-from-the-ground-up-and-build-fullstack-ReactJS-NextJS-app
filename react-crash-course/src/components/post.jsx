63991f03aad40885254c7578943680d498f55759
sayantan pal <sayantan.pal@prismforce.ai>
2025-04-28 13:00:35 +0530

    implemented  props along with fetching post class name from css file

--- react-crash-course/src/components/post.jsx
+++ react-crash-course/src/components/post.jsx
@@ -1,7 +1,7 @@
-
-function Post(props){
+import classes from './post.module.css';
+function Post(props){     // props is an object that contains all the data passed to this component from its parent (the App component)
     
-    return (<div>
+    return (<div className={classes.post}> // post is a class name in css file
         <p>{props.author}</p>
         <p>{props.body}</p>
         </div>)
